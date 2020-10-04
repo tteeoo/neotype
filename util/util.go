7c3d01ffc85ad524ad7f4cc00d5f438b4c3cbc87
Jacob Colvin <jacobcolvin1@gmail.com>
2020-10-03 23:26:03 -0400

    Simplify ResolveShare logic

--- util/util.go
+++ util/util.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"path/filepath"
 )
 
 // DieIf printfs and exits if err != nil.
@@ -17,27 +18,30 @@ func DieIf(err error, format string, a ...interface{}) {
 // ResolveShare checks multiple environment variables, finds the location of the data directory,
 // ensures it exists and it is accessable, then returns the path.
 func ResolveShare() (string, error) {
-	share := os.Getenv("NEOTYPE_DATA")
-	if share == "" {
-		share = os.Getenv("XDG_DATA_HOME")
-		if share == "" {
-			user, err := user.Current()
-			if err != nil {
-				return "", err
-			}
-			share = user.HomeDir + "/.local/share/neotype"
-		} else {
-			share += "/neotype"
+	neotypeData := os.Getenv("NEOTYPE_DATA")
+	xdgDataHome := os.Getenv("XDG_DATA_HOME")
+
+	var share string
+	if neotypeData != "" {
+		share = neotypeData
+	} else if xdgDataHome != "" {
+		share = filepath.Join(xdgDataHome, "/neotype")
+	} else {
+		user, err := user.Current()
+		if err != nil {
+			return "", err
 		}
+		share = filepath.Join(user.HomeDir, "/.local/share/neotype")
 	}
+
 	_, err := os.Stat(share)
 	if os.IsNotExist(err) {
-		err = os.Mkdir(share, 0755)
-		if err != nil {
+		if err = os.Mkdir(share, 0755); err != nil {
 			return "", err
 		}
 	} else if err != nil {
 		return "", err
 	}
+
 	return share, nil
 }
