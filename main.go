cba3285fd84af042a30b76b34ccfa131a3edbc8d
Theo Henson <theodorehenson@protonmail.com>
2020-10-15 01:48:09 -0400

    Implement textfile option

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 var version = flag.Bool("version", false, "Print version information and exit.")
 var words = flag.Int("words", 20, "The number of words to test with.")
 var wordFile = flag.String("wordfile", "words.txt", "The path to the file of newline-separated words to use.\nThis can be an absolute or relative path.\nIf it is invalid it will be treated as a relative path from the data directory.")
+var textFile = flag.String("textfile", "", "The path to a file containing exactly what to type.\nThe path works the same as wordfile. (overrides wordfile)")
 
 func main() {
 
@@ -33,33 +34,48 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Get word file
-	wordFilePath, err := util.ResolveFilePath(*wordFile)
-	util.DieIf(err, "NeoType: Error: Cannot find word file: %s\n", err)
-
 	// Get terminal dimensions
 	w, h, err := terminal.GetSize(0)
 	util.DieIf(err, "NeoType: Error: Cannot get terminal size: %s\n", err)
 
-	// Generate words
-	dictionaryB, err := ioutil.ReadFile(wordFilePath)
-	util.DieIf(err, "NeoType: Error: Cannot read file \"%s\": %s\n", wordFilePath, err)
-	dictionary := strings.Split(string(dictionaryB), "\n")
-	var fixedDictionary []string
-	for _, v := range dictionary {
-		if v != "" {
-			fixedDictionary = append(fixedDictionary, v)
+	// Get word source
+	var wordString string
+
+	if *textFile == "" {
+		// Get word file
+		wordFilePath, err := util.ResolveFilePath(*wordFile)
+		util.DieIf(err, "NeoType: Error: Cannot find word file: %s\n", err)
+
+		// Generate words
+		dictionaryB, err := ioutil.ReadFile(wordFilePath)
+		util.DieIf(err, "NeoType: Error: Cannot read file \"%s\": %s\n", wordFilePath, err)
+		dictionary := strings.Split(string(dictionaryB), "\n")
+		var fixedDictionary []string
+		for _, v := range dictionary {
+			if v != "" {
+				fixedDictionary = append(fixedDictionary, v)
+			}
 		}
-	}
-	rand.Seed(time.Now().Unix())
-	var chosen []string
-	for i := 0; i < *words; i++ {
-		chosen = append(chosen, fixedDictionary[rand.Intn(len(fixedDictionary))])
+		rand.Seed(time.Now().Unix())
+		var chosen []string
+		for i := 0; i < *words; i++ {
+			chosen = append(chosen, fixedDictionary[rand.Intn(len(fixedDictionary))])
+		}
+		wordString = strings.Join(chosen, " ")
+	} else {
+		textFilePath, err := util.ResolveFilePath(*textFile)
+		util.DieIf(err, "NeoType: Error: Cannot find text file: %s\n", err)
+
+		b, err := ioutil.ReadFile(textFilePath)
+		util.DieIf(err, "NeoType: Error: Cannot read file: %s\n", err)
+
+		// TODO: newline support
+		wordString = strings.ReplaceAll(string(b), "\n", " ")
 	}
 
 	// Declare Game
 	g := game.Game{
-		WordString: strings.ReplaceAll(strings.Join(chosen, " "), "\t", "    "),
+		WordString: strings.ReplaceAll(wordString, "\t", "    "),
 		Width:      w,
 		Height:     h,
 	}
